d67cc99598700646f59899f79c127922d82d8bb9
Tim Speelman <timspeelman@live.nl>
2020-07-03 20:00:56 +0200

    Add reload to contactdetailpage

--- src/pages/ContactDetailPage.tsx
+++ src/pages/ContactDetailPage.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Button } from "../components/Button";
 import { CredentialCard } from "../components/CredentialCard";
 import { SubpageHeader } from "../components/SubpageHeader";
 import { useInternationalization } from "../hooks/useInternationalization";
 import { useSelector } from "../hooks/useSelector";
+import { useServices } from "../hooks/useServices";
 import { getProviderByMid } from "../services/local/selectors";
 
 export const ContactDetailPage: React.FC<Props> = ({ providerMid }) => {
 
     const { fromLanguageDict } = useInternationalization();
-
+    const { services } = useServices();
     const provider = useSelector(getProviderByMid(providerMid));
+
+    const reloadProvider = (e: any) => {
+        e.preventDefault();
+        services!.providersService?.addByURL(provider!.url).then(() => {
+            console.log("Reloaded provider")
+        }).catch()
+    }
 
     return !provider ? <div>Oops! Provider not found</div> : (
 
@@ -23,6 +32,7 @@ export const ContactDetailPage: React.FC<Props> = ({ providerMid }) => {
                 <img src={provider.logo_url} alt={fromLanguageDict(provider.title)} />
                 <h1>{fromLanguageDict(provider.title)}</h1>
                 <p>{fromLanguageDict(provider.description)}</p>
+                <Button onClick={reloadProvider}>Reloads</Button>
                 <table>
                     <tbody>
                         <tr>
